31dde5503fbd301823ea1045ac5eb0a87c67bbfa
Gus Class <class@google.com>
2013-04-22 21:32:07 -0700

    Cleaned up code

--- js/ui.js
+++ js/ui.js
@@ -1,66 +1,80 @@
-/* ui.js - functionality related to UI */
+  /* ui.js - functionality related to UI */
 
-// updateProgress - updates the UI and determines whether the chart should be rendered
-    function updateProgress(){
-      var countsLeft = 0;
-      for (var i=0; i < progressTracking.length; i++){
-        countsLeft += progressTracking[i];
-      }
-      if (countsLeft > 0){
-        if (debug > 1){
-          console.log("not finished, continuing");
-        }
-
-        console.log(document.getElementById("theProgressBar"));
-
-        var percent = 100 - Math.floor((countsLeft / (maxQueryCount * hashTags.length)) * 100);
-        var progressBar = document.getElementById("theProgressBar");
-
-        if (strategy == "sample"){
-          // TODO: if this makes sense, should just count # samples/sampleLimit
-          //percent = 
-        }
-
-        document.getElementById("theProgressBar").style.width = percent + "%";
-
-      }else{
-        renderContent();
-      }
+  /**
+    * updateProgress 
+    * updates the UI and determines whether the chart should be rendered.
+    */
+  function updateProgress(){
+    var countsLeft = 0;
+    for (var i=0; i < progressTracking.length; i++){
+      countsLeft += progressTracking[i];
     }
-
-// toggleStrategy - changes the strategy used for collecting samples/posts/activities
-    function toggleStrategy(){
-      if (strategy == "chron"){
-        strategy = "sample";
-      }else{
-        theCurveType = "chron";
+    if (countsLeft > 0){
+      if (debug > 1){
+        console.log("not finished, continuing");
       }
-    }
 
-// toggleLineType - turns on the silly curved lines for charts
-    function toggleLineType(){
-      if (theCurveType == "function"){
-        theCurveType = "none";
-      }else{
-        theCurveType = "function";
+      console.log(document.getElementById("theProgressBar"));
+
+      var percent = 100 - Math.floor((countsLeft / (maxQueryCount * hashTags.length)) * 100);
+      var progressBar = document.getElementById("theProgressBar");
+
+      if (strategy == "sample"){
+        // TODO: if this makes sense, should just count # samples/sampleLimit
+        //percent = 
       }
-    }
 
-// updateHoursAgo - sets the # of hours to sample data for when using the 
-//   chronological strategy
-    function updateHoursAgo(){
-      var hours    = parseInt(document.getElementById("thehours").value);
-      timeAgoLimit = hours * 60;
-      splitTime    = timeAgoLimit / buckets;
-      if (debug > 2){
-        console.log("timeAgoLimit now: " + timeAgoLimit);
-        console.log("splitTime now: " + splitTime);
-      }
-    }
+      document.getElementById("theProgressBar").style.width = percent + "%";
 
-// updateSampleLimit - sets the # of samples to retrieve when using the 
-//    sample strategy
-    function updateSampleLimit(){
-      sampleLimit = parseInt(document.getElementById("samplesLimit").value);
-
+    }else{
+      renderContent();
     }
+  }
+  /**
+    * toggleStrategy 
+    * Changes the strategy used for collecting samples/posts/activities
+    */
+  function toggleStrategy(){
+    if (strategy == "chron"){
+      strategy = "sample";
+    }else{
+      theCurveType = "chron";
+    }
+  }
+
+  /**
+    * toggleLineType 
+    * Turns on the silly curved lines for charts.
+    */
+  function toggleLineType(){
+    if (theCurveType == "function"){
+      theCurveType = "none";
+    }else{
+      theCurveType = "function";
+    }
+  }
+
+  /**
+    * updateHoursAgo 
+    * Sets the # of hours to sample data for when using the 
+    * chronological strategy
+    */
+  function updateHoursAgo(){
+    var hours    = parseInt(document.getElementById("thehours").value);
+    timeAgoLimit = hours * 60;
+    splitTime    = timeAgoLimit / buckets;
+    if (debug > 2){
+      console.log("timeAgoLimit now: " + timeAgoLimit);
+      console.log("splitTime now: " + splitTime);
+    }
+  }
+
+  /**
+    * updateSampleLimit 
+    * Sets the # of samples to retrieve when using the 
+    * sample strategy.
+    */
+  function updateSampleLimit(){
+    sampleLimit = parseInt(document.getElementById("samplesLimit").value);
+
+  }
--- js/utility.js
+++ js/utility.js
@@ -1,8 +1,14 @@
 /* utility.js - Various utility functions */
 
-
-// findMaxCount - determine the largest bucket of 
-// entries
+    /**
+      * findMaxCount 
+      * Determine the largest bucket of entries
+      *
+      * @param buckets
+      *   An array containing counts of entries.
+      *
+      * @return The maximum value in the array.
+      */
     function findMaxCount(buckets){
       var maxValue = 0;
       for (var index in buckets){
@@ -13,7 +19,15 @@
       return maxValue;
     }
 
-// findOldestEntry - find the oldest value in a series
+    /**
+      * findOldestEntry 
+      * Find the oldest value in a series
+      *
+      * @param someSeries
+      *   The chart series [series][bucket][millis] to find the oldest entry of.
+      *
+      * @return The maximum value from the chart series.
+      */
     function findOldestEntry(someSeries){
       var maxValue = 0;
       for (seriesNum in someSeries){
@@ -27,8 +41,14 @@
       return maxValue;
     }
 
-// From stack overflow / blog.stevenlevithan.com
-// a javascript trim function
+    /**
+      * From stack overflow / blog.stevenlevithan.com
+      * a javascript trim function
+      *
+      * @param str The string to trim.
+      *
+      * @return The trimmed string.
+      */
     function trim (str) {
         str = str.replace(/^\s+/, '');
         for (var i = str.length - 1; i >= 0; i--) {
@@ -40,8 +60,14 @@
         return str;
     }
 
-// getChartDateTime - turn milliseconds limits into entries
-// to be used on chart
+    /**
+      * getChartDateTime 
+      * Turn milliseconds limits into entries to be used on chart
+      *
+      * @param Time in milliseconds.
+      *
+      * @return Formatted date.
+      */
     function getChartDateTime(millis){
       var days = ["Sun","Mon","Tues","Wed","Thur","Fri","Sat"];
       var thisDate = new Date(now - millis);
